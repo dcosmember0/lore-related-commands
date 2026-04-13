52c56064e76d321439a1c30e9e29e5369844961a
dcoszero0 <dcoszero0@gmail.com>
2026-04-13 11:44:02 -0400

    added a dot-art start frame on the instability.sh file and a few other small edits

--- instability.sh
+++ instability.sh
@@ -1,4 +1,13 @@
 clear
+cat symbol3.txt
+sleep 2.5
+clear
+cat symbol1.txt
+sleep 0.5
+clear
+cat logo1.txt
+sleep 3
+clear
 sleep 3
 cat blame-him.txt | while read line; do echo "$line"; sleep 1.5; done
 cat insanity.txt | while read line; do echo "$line"; sleep 0.25; done
