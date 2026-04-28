9a6ca26d6a26109db5b790dc2d7b86518778c255
dcoszero0 <dcoszero0@gmail.com>
2026-04-28 12:31:28 -0400

    added a bit of extra lore

---
+++ instability2.sh
@@ -0,0 +1,8 @@
+clear
+cat symbol1.txt
+sleep 0.5
+clear
+sleep 3
+cat blame-him.txt | while read line; do echo "$line"; sleep 1.5; done
+cat insanity.txt | while read line; do echo "$line"; sleep 0.25; done
+./instability2.sh
